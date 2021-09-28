618743f2da41d52cd7b98fbf9a5c0815c3738c60
Andrea Prouvost <andrea.prouvost@gmail.com>
2021-09-28 12:30:08 +0200

    models associations and validations ok

--- app/models/fourniture.rb
+++ app/models/fourniture.rb
@@ -1,3 +1,7 @@
 class Fourniture < ApplicationRecord
   belongs_to :room
+  validates :name, presence: true, uniqueness: { scope: :room_id }
+  validates :image_url, presence: true
+  URL_REGEXP = /(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?/ix
+  validates :image_url, format: { with: URL_REGEXP, message: 'format invalid' }
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,7 @@
 class Room < ApplicationRecord
+  has_many :fournitures, dependent: :destroy
+  validates :name, presence: true, uniqueness: true
+  validates :banner_url, presence: true
+  URL_REGEXP = /(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?/ix
+  validates :banner_url, format: { with: URL_REGEXP, message: 'format invalide' }
 end
